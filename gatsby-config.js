3e09fb61ebfc453bafa21d1b3a564e66db76e410
Chris Hastings-Spital <chrismhs@gmail.com>
2020-04-16 17:16:34 +0100

    Updated tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-131855428-1",
+        trackingId: "UA-163826436-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
